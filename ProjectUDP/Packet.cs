997fcf58812a7a34aa32ec592bdf5eaf5beecb5a
Filip Wicha <5021567@gmail.com>
2017-12-05 17:37:50 +0100

    packet ultra turbo short

--- ProjectUDP/Packet.cs
+++ ProjectUDP/Packet.cs
@@ -6,7 +6,64 @@ using System.Threading.Tasks;
 
 namespace ProjectUDP
 {
+    public enum Answer
+    {
+        Guessed,
+        notGuessed
+    }
+
     class Packet
     {
+
+        System.Collections.Generic.Dictionary<string, int> collection = new Dictionary<string, int>();
+
+        int leftNumber; //or left lim of range
+        Answer answer;
+        int sessionId;
+        int rightNumber;
+
+        StringBuilder datagram;
+        
+        public Packet()
+        {
+            datagram = new StringBuilder();
+
+        }
+
+        public Packet(byte[] received)
+        {
+            datagram = append(Encoding.ASCII.GetString(received));
+        }
+
+        byte[] GetBytes()
+        {
+            return Encoding.ASCII.GetBytes(datagram.ToString());
+        }
+
+
+        void Serialize()
+        {
+            collection.Add("LeftNumber", leftNumber);
+            collection.Add("Answer", (int)answer);
+            collection.Add("SessionID", sessionId);
+            collection.Add("RightNumber", rightNumber);
+
+            datagram.Append("#");
+
+            foreach (var item in collection)
+            {
+                datagram.Append(item.Key + "#$#" + item.Value + "#");
+            }
+        }
+
+        void Deserialize()
+        {
+            string message = "#LeftNumber#$#5#Answer#$#0#SesionID#$#4424412#RightNumber#$#5#";
+            Int32.TryParse(System.Text.RegularExpressions.Regex.Match(message, @"(?<=#LeftNumber#\$#)[0-9]+").Value, leftNumber);
+            Int32.TryParse(System.Text.RegularExpressions.Regex.Match(message, @"(?<=#Answer#\$#)[0-9]+").Value, answer);
+            Int32.TryParse(System.Text.RegularExpressions.Regex.Match(message, @"(?<=#SessionID#\$#)[0-9]+").Value, sessionId);
+            Int32.TryParse(System.Text.RegularExpressions.Regex.Match(message, @"(?<=#RightNumber#\$#)[0-9]+").Value, rightNumber);
+        }
+
     }
 }
